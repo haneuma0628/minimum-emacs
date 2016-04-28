01b6769f0145555e5df68bb890ba5bb6bf0b5560
hnmx4 <haneuma0628@gmail.com>
2016-04-29 05:37:15 +0900

    外部パッケージをなくしてすぐ使えるinit.elにした

--- init.el
+++ init.el
@@ -8,20 +8,8 @@
         (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
-
-(add-to-load-path "elpa")
-
-
-;;; パッケージ管理
-(require 'package)
-
-; Add package-archives
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) ; ついでにmarmaladeも追加
-
-; Initialize
-(package-initialize)
-
+;; usage
+;; (add-to-load-path "path-name")
 
 ;;; 機能いろいろ
 (setq make-backup-file nil)
@@ -33,7 +21,7 @@
 
 
 ;;; Emacsの見た目
-(load-theme 'adwaita t)
+(load-theme 'wombat t)
 (menu-bar-mode -1) ; メニューバーを消す
 (tool-bar-mode -1) ; ツールバーを消す
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
@@ -62,59 +50,10 @@
 (setq backup-inhibited t)
 
 
-;;; python-mode
-(add-hook 'python-mode-hook
-          (lambda ()
-            ;; use tab-indent
-            (setq indent-tabs-mode t)
-            (setq python-indent-offset 8)))
-(put 'set-goal-column 'disabled nil)
-
-
-;;; タブエディタっぽくできる tabbar
-(require 'tabbar)
-(tabbar-mode 1)
-
-;; グループ化しない
-(setq tabbar-buffer-groups-function nil)
-
-;; 左に表示されるボタンを無効化
-(dolist (btn '(tabbar-buffer-home-button
-               tabbar-scroll-left-button
-               tabbar-scroll-right-button))
-  (set btn (cons (cons "" nil)
-                 (cons "" nil))))
-
-;; タブ同士の間隔
-(setq tabbar-separator '(1.5))
-
-;; 外観変更
-(set-face-attribute
- 'tabbar-default nil
- :family (face-attribute 'default :family)
- :background (face-attribute 'mode-line-inactive :background)
- :height 0.9)
-(set-face-attribute
- 'tabbar-unselected nil
- :background (face-attribute 'mode-line-inactive :background)
- :foreground (face-attribute 'mode-line-inactive :foreground)
- :box nil)
-(set-face-attribute
- 'tabbar-selected nil
- :background (face-attribute 'mode-line :background)
- :foreground (face-attribute 'mode-line :foreground)
- :box nil)
-(defun get-color-helper (face attribute n diff)
-  (format "%x" (max 0 (min (car (color-values "white"))
-                           (+ diff (nth n (color-values (face-attribute face attribute))))))))
-(defun create-color-helper (face attribute r g b)
-  (concat "#"
-          (get-color-helper face attribute 0 r)
-          (get-color-helper face attribute 1 g)
-          (get-color-helper face attribute 2 b)))
-;; usage
-(create-color-helper 'mode-line :foreground 10 -20 30)
-
-;; キーに割り当てる
-(global-set-key [C-tab] 'tabbar-forward-tab)
-(global-set-key [C-S-tab] 'tabbar-backward-tab)
+;;; coding example:python-mode
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             ;; use tab-indent
+;;             (setq indent-tabs-mode t)
+;;             (setq python-indent-offset 8)))
+;; (put 'set-goal-column 'disabled nil);
